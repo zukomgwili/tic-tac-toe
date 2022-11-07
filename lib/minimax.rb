a8a878bba55223852026f658619f9df0066099ac
Zuko Mgwili <zmgwili@8thlight.com>
2022-11-07 14:46:48 +0000

    chore: test b-trees with a depth of two

--- lib/minimax.rb
+++ lib/minimax.rb
@@ -1,7 +1,15 @@
 def minimax(leaves, is_maximizer)
   if is_maximizer
-    leaves.max
+    return leaves.max if leaves.length == 2
+
+    chunks = leaves.each_slice(2).to_a
+    reduced = chunks.reduce([]) { |acc, array| acc << array.min }
+    return reduced.max if reduced.length == 2
   else
-    leaves.min
+    return leaves.min if leaves.length == 2
+
+    chunks = leaves.each_slice(2).to_a
+    reduced = chunks.reduce([]) { |acc, array| acc << array.max }
+    return reduced.min if reduced.length == 2
   end
 end
--- spec/minimax_spec.rb
+++ spec/minimax_spec.rb
@@ -4,16 +4,32 @@ describe 'minimax' do
   describe 'given a binary tree with a depth of one' do
     describe "when it is the maximizer's turn" do
       it 'should return the larger leaf value' do
-        result = minimax([3, 5, 2], true)
+        result = minimax([3, 5], true)
 
         expect(result).to eq(5)
       end
     end
     describe "when it is the minimizer's turn" do
       it 'should return the smaller leaf value' do
-        result = minimax([3, 5, 4], false)
+        result = minimax([3, 5], false)
 
         expect(result).to eq(3)
+      end
+    end
+  end
+  describe 'given a binary tree with a depth of two' do
+    describe "when the maximizer looks ahead to see the minimizer's optimal moves" do
+      it "should get the larger of the minimizer's moves" do
+        result = minimax([3, 6, 2, 4], true)
+
+        expect(result).to eq(3)
+      end
+    end
+    describe "when the minimizer looks ahead to see the maximizer's optimal moves" do
+      it "should get the smaller of the maximizer's moves" do
+        result = minimax([3, 5, 2, 4], false)
+
+        expect(result).to eq(4)
       end
     end
   end
