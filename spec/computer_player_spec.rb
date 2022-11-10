1ebbc590c3673f446d83706f2fbd817db8e5344d
Zuko Mgwili <zmgwili@8thlight.com>
2022-11-10 11:24:17 +0000

    feat: return 1 if player wins left column

--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -94,5 +94,23 @@ describe ComputerPlayer do
         expect(result).to eq(-1)
       end
     end
+    describe 'when the player has a winning left column' do
+      it 'should return the value 1' do
+        player = ComputerPlayer.new('X')
+
+        result = player.evaluate(['X', 'O', 'O', 'X', '', '', 'X', '', ''])
+
+        expect(result).to eq(1)
+      end
+    end
+    skip 'when the opponent has a winning left column' do
+      it 'should return the value -1' do
+        player = ComputerPlayer.new('X')
+
+        result = player.evaluate(['', 'X', 'X', 'X', '', '', 'O', 'O', 'O'])
+
+        expect(result).to eq(-1)
+      end
+    end
   end
 end
