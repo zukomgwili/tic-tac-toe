bb21ce0a166e0d52341f0156e0d30cd813460988
Zuko Mgwili <zmgwili@8thlight.com>
2022-11-10 11:31:00 +0000

    feat: return 1 if player wins middle column

--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -112,5 +112,23 @@ describe ComputerPlayer do
         expect(result).to eq(-1)
       end
     end
+    describe 'when the player has a winning middle column' do
+      it 'should return the value 1' do
+        player = ComputerPlayer.new('X')
+
+        result = player.evaluate(['O', 'X', 'O', 'O', 'X', '', '', 'X', ''])
+
+        expect(result).to eq(1)
+      end
+    end
+    skip 'when the opponent has a winning middle column' do
+      it 'should return the value -1' do
+        player = ComputerPlayer.new('X')
+
+        result = player.evaluate(['', 'X', 'X', 'X', '', '', 'O', 'O', 'O'])
+
+        expect(result).to eq(-1)
+      end
+    end
   end
 end
