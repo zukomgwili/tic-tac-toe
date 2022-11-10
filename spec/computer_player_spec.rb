9c938fa0c9002cdebb04d93aaa27948dd4bca1b0
Zuko Mgwili <zmgwili@8thlight.com>
2022-11-10 11:46:18 +0000

    feat:return -1 if opponent wins -gradient diagonal

--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -157,11 +157,11 @@ describe ComputerPlayer do
         expect(result).to eq(1)
       end
     end
-    skip 'when the opponent has a winning negative gradient diagonal' do
+    describe 'when the opponent has a winning negative gradient diagonal' do
       it 'should return the value -1' do
         player = ComputerPlayer.new('X')
 
-        result = player.evaluate(['X', 'X', 'O', 'X', '', 'O', '', '', 'O'])
+        result = player.evaluate(['O', 'X', '', 'X', 'O', 'X', '', '', 'O'])
 
         expect(result).to eq(-1)
       end
